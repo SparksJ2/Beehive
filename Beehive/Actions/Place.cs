b0cdddd738b9f6909c01dd25d2e56eaa5a997848
sparks <sparksj0002@gmail.com>
2018-03-05 18:42:50 +0000

    moved throw and place actions out of player main file

---
+++ Beehive/Actions/Place.cs
@@ -0,0 +1,74 @@
+﻿namespace Beehive
+{
+	partial class Player
+	{
+		private void PlaceItemNorth()
+		{
+			MapTile t = Refs.m.TileByLoc(loc).OneNorth();
+			PlaceItemOnTile(t);
+		}
+
+		private void PlaceItemEast()
+		{
+			MapTile t = Refs.m.TileByLoc(loc).OneEast();
+			PlaceItemOnTile(t);
+		}
+
+		private void PlaceItemSouth()
+		{
+			MapTile t = Refs.m.TileByLoc(loc).OneSouth();
+			PlaceItemOnTile(t);
+		}
+
+		private void PlaceItemWest()
+		{
+			MapTile t = Refs.m.TileByLoc(loc).OneWest();
+			PlaceItemOnTile(t);
+		}
+
+		private void PlaceItemOnTile(MapTile t)
+		{
+			if (Refs.m.EdgeLoc(t.loc)) return;
+
+			if (heldCubiId > 0 && t.clear && !Refs.m.ContainsCubi(t.loc))
+			{
+				// put down lover
+				Cubi myHeldCubi = Harem.GetId(heldCubiId);
+				myHeldCubi.loc = t.loc;
+				myHeldCubi.beingCarried = false;
+				heldCubiId = 0;
+				Refs.mf.Announce("You're free to go... if you can.", myAlign, myColor);
+			}
+			else if (heldCubiId == 0 && Refs.m.ContainsCubi(t.loc))
+			{
+				// pick up lover
+				Cubi caughtCubi = Refs.m.CubiAt(t.loc);
+				caughtCubi.loc = this.loc;
+				caughtCubi.beingCarried = true;
+				heldCubiId = caughtCubi.myIdNo;
+				Refs.mf.Announce("Gotcha!", myAlign, myColor);
+				Refs.mf.Announce("EEEK!!", caughtCubi.myAlign, caughtCubi.myColor);
+			}
+			else if (t.clear && heldPillows > 0 && !Refs.m.ContainsCubi(t.loc))
+			{
+				// make wall
+				t.clear = false;
+				heldPillows--;
+				Refs.mf.Announce("You place the pillow to make a wall. You have " + heldPillows + " left.", myAlign, myColor);
+			}
+			else if (heldCubiId == 0 && !t.clear)
+			{
+				// clear wall
+				t.clear = true;
+				heldPillows++;
+				Refs.mf.Announce("You pick up a pillow. You now have " + heldPillows + ".", myAlign, myColor);
+			}
+			else
+			{
+				// can't figure out when they want to do, so...
+				Refs.mf.Announce("Not enough space for that here.", myAlign, myColor);
+			}
+			UpdateInventory();
+		}
+	}
+}
---
+++ Beehive/Actions/Throw.cs
@@ -0,0 +1,136 @@
+﻿using System.Threading;
+
+namespace Beehive
+{
+	partial class Player
+	{
+		private void ThrowPillowNorth()
+		{
+			MapTile t = Refs.m.TileByLoc(this.loc).OneNorth();
+			if (t.clear) ThrowPillow(Dir.North);
+		}
+
+		private void ThrowPillowWest()
+		{
+			MapTile t = Refs.m.TileByLoc(this.loc).OneWest();
+			if (t.clear) ThrowPillow(Dir.West);
+		}
+
+		private void ThrowPillowEast()
+		{
+			MapTile t = Refs.m.TileByLoc(this.loc).OneEast();
+			if (t.clear) ThrowPillow(Dir.East);
+		}
+
+		private void ThrowPillowSouth()
+		{
+			MapTile t = Refs.m.TileByLoc(this.loc).OneSouth();
+			if (t.clear) ThrowPillow(Dir.South);
+		}
+
+		private void Animate(MapTile activeTile, string pillowGlyph)
+		{
+			// todo animation code is a bit makeshift and needs to be cleaned up andmoved to Map.cs
+			activeTile.clear = false;
+			activeTile.gly = pillowGlyph;
+
+			Refs.mf.UpdateMap();
+
+			Thread.Sleep(75);
+			activeTile.clear = true;
+			activeTile.gly = " ";
+			Refs.mf.UpdateMap();
+		}
+
+		private void ThrowPillow(Loc vector)
+		{
+			Refs.mf.Announce("You throw a pillow!", myAlign, myColor);
+			// can't throw without pillow!
+			if (heldPillows <= 0)
+			{ return; }
+			else
+			{ heldPillows--; UpdateInventory(); }
+
+			// determine release point of throw
+			Loc startloc = Loc.AddPts(this.loc, vector);
+			MapTile activeTile = Refs.m.TileByLoc(startloc);
+			string pillowGlyph = "O";
+
+			// if the next tile now is our lover, extra spank stun!
+			string moveClear = CheckClearForThrown(vector, activeTile);
+			if (moveClear == "spank")
+			{
+				MapTile victimTile = Refs.m.TileByLoc(Loc.AddPts(activeTile.loc, vector));
+				Cubi victim = Refs.m.CubiAt(victimTile.loc);
+
+				Refs.mf.Announce("POINT BLANK PILLOW SPANK!", myAlign, myColor);
+				victim.Spank(5);
+				Refs.mf.Announce("oww! *moan*", victim.myAlign, victim.myColor);
+			}
+
+			while (moveClear == "clear")
+			{
+				// blip activeTile with pillow symbol
+				Animate(activeTile, pillowGlyph);
+
+				// is the next tile clear?
+				moveClear = CheckClearForThrown(vector, activeTile);
+
+				// nope, it has your cubi in.
+				if (moveClear == "spank")
+				{
+					MapTile victimTile = Refs.m.TileByLoc(Loc.AddPts(activeTile.loc, vector));
+					Cubi victim = Refs.m.CubiAt(victimTile.loc);
+
+					MapTileSet escapes = new MapTileSet();
+
+					if (IsVertical(vector))
+					{
+						escapes = victimTile.GetPossibleMoves(Dir.DodgeVertical);
+					}
+					else
+					{
+						escapes = victimTile.GetPossibleMoves(Dir.DodgeHorizontal);
+					}
+
+					if (escapes.Count > 0)
+					{
+						victim.loc = MapTile.RandomFromList(escapes).loc;
+						Refs.mf.Announce("Nyahhh missed me!", victim.myAlign, victim.myColor);
+						moveClear = "clear";
+					}
+					else
+					{
+						victim.Spank(5);
+						Refs.mf.Announce("Owwwww!", victim.myAlign, victim.myColor);
+					}
+				}
+
+				// just a wall. stop here.
+				if (moveClear == "wall")
+				{ Refs.mf.Announce("You didn't hit anything interesting.", myAlign, myColor); }
+
+				// it's clear, so move activeTile up and iterate
+				if (moveClear == "clear")
+				{ activeTile = Refs.m.TileByLoc(Loc.AddPts(vector, activeTile.loc)); }
+			}
+			// leave pillow on ground to form new obstruction
+			activeTile.clear = false;
+			Refs.m.HealWalls();
+			Refs.mf.UpdateMap();
+		}
+
+		private string CheckClearForThrown(Loc vector, MapTile activeTile)
+		{
+			Loc newloc = Loc.AddPts(vector, activeTile.loc);
+			if (!Refs.m.TileByLoc(newloc).clear) return "wall";
+
+			foreach (Cubi c in Refs.h.roster)
+			{
+				if (Refs.m.TileByLoc(newloc).loc == c.loc) { return "spank"; }
+			}
+
+			return "clear";
+		}
+	}
+}
