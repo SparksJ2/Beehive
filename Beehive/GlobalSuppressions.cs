35575aeb78984a18818d6c126c5d0f8232cc92ee
sparks <sparksj0002@gmail.com>
2018-06-29 21:14:23 +0100

    more buggy IDE0044 supression

--- Beehive/GlobalSuppressions.cs
+++ Beehive/GlobalSuppressions.cs
@@ -4,5 +4,8 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "Bug", Scope = "member", Target = "~F:Beehive.FlowMap.level")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Bug>", Scope = "member", Target = "~F:Beehive.FlowMap.level")]
 
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Bug>", Scope = "member", Target = "~F:Beehive.MainMap.TileDesc.s")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Bug>", Scope = "member", Target = "~F:Beehive.MainMap.TileDesc.c")]
