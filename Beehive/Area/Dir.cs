251340ca921f216d9a81468772ba83e1f967c469
sparks <sparksj0002@gmail.com>
2019-01-01 20:50:17 +0000

    direction updates

--- Beehive/Area/Dir.cs
+++ Beehive/Area/Dir.cs
@@ -11,19 +11,38 @@ namespace Beehive
 		public static Loc South = new Loc(0, 1);
 		public static Loc West = new Loc(-1, 0);
 
+		public static Loc North2 = new Loc(0, -2);
+		public static Loc East2 = new Loc(2, 0);
+		public static Loc South2 = new Loc(0, 2);
+		public static Loc West2 = new Loc(-2, 0);
+
 		public static Loc NorthEast = Loc.AddPts(North, East);
 		public static Loc SouthEast = Loc.AddPts(South, East);
 		public static Loc SouthWest = Loc.AddPts(South, West);
 		public static Loc NorthWest = Loc.AddPts(North, West);
+
+		public static Loc NorthEast2 = Loc.AddPts(North2, East2);
+		public static Loc SouthEast2 = Loc.AddPts(South2, East2);
+		public static Loc SouthWest2 = Loc.AddPts(South2, West2);
+		public static Loc NorthWest2 = Loc.AddPts(North2, West2);
 
 		public static HashSet<Loc> AllAround =
 			new HashSet<Loc> {
 				North, East, South, West,
 			 	NorthEast, SouthEast, SouthWest, NorthWest };
 
+		public static HashSet<Loc> AllAround2 =
+			new HashSet<Loc> {
+				North2, East2, South2, West2,
+			 	NorthEast2, SouthEast2, SouthWest2, NorthWest2 };
+
 		public static HashSet<Loc> Cardinals =
 			new HashSet<Loc> {
 				North, East, South, West };
+
+		public static HashSet<Loc> Cardinals2 =
+			new HashSet<Loc> {
+				North2, East2, South2, West2 };
 
 		public static HashSet<Loc> KnightMoves =
 			new HashSet<Loc> {
@@ -35,15 +54,17 @@ namespace Beehive
 		public static HashSet<Loc> DodgeHorizontal =
 			new HashSet<Loc> { North, South };
 
-		public static HashSet<Loc> DodgeMoves =
-			new HashSet<Loc> { North, East, South, West };
+		public static HashSet<Loc> DodgeHorizontal2 =
+			new HashSet<Loc> { North2, South2 };
 
 		public static HashSet<Loc> DodgeVertical =
 			new HashSet<Loc> { East, West };
 
-		public static HashSet<Loc> LeapMoves =
-			new HashSet<Loc> {
-				Loc.AddPts(North, North), Loc.AddPts(East, East),
-				Loc.AddPts(South, South), Loc.AddPts(West, West)};
+		public static HashSet<Loc> DodgeVertical2 =
+			new HashSet<Loc> { East2, West2 };
+
+		public static HashSet<Loc> DodgeMoves = Cardinals;
+
+		public static HashSet<Loc> LeapMoves = Cardinals2;
 	}
 }
